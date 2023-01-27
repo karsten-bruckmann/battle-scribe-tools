6b97167aaa45af42b669dcd53c2e0c20e6b464de
Karsten Bruckmann <karsten.bruckmann@check24.de>
2023-01-27 20:32:55 +0100

    [main] rename app

--- apps/flash-cards/jest.config.ts
+++ apps/flash-cards/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'flash-card-generator',
+  displayName: 'flash-cards',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/flash-card-generator',
+  coverageDirectory: '../../coverage/apps/flash-cards',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
