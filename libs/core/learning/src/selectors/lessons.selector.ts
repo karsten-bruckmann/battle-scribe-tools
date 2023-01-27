c56cff64d202270e5d422fc6c5dbe3127d8c115d
Karsten Bruckmann <karsten.bruckmann@check24.de>
2023-01-27 21:21:37 +0100

    [main] show lessons

--- libs/core/learning/src/selectors/lessons.selector.ts
+++ libs/core/learning/src/selectors/lessons.selector.ts
@@ -1,6 +1,18 @@
 import { flashCardsSelector } from '@battle-scribe-tools/data-access/flash-cards';
 import { createSelector } from '@ngrx/store';
+import { Lesson } from '../models/lesson.model';
 
-export const lessonsSelector = createSelector(flashCardsSelector, (state) =>
-  state.map((deck) => deck.name)
+export const lessonsSelector = createSelector(
+  flashCardsSelector,
+  (state): Lesson[] =>
+    state.map((deck) => ({
+      name: deck.name,
+      boxContent: deck.cards.reduce<number[]>((content, card) => {
+        if (!content[card.box]) {
+          content[card.box] = 0;
+        }
+        content[card.box]++;
+        return content;
+      }, new Array(deck.boxes).fill(0)),
+    }))
 );
